43ded5b2a03195b007ea906476e5fb3dc8a8c197
philipptrenz <mail@philipptrenz.de>
2016-12-19 10:05:48 +0100

    minor changes

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# run photo booth and restart for the case it crashed
+# run photo booth and restart for the case it crashes
 while true; do
 	sudo startx ~/photo-booth/node_modules/.bin/electron ~/photo-booth/ -- -nocursor
 done

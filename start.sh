f3c3377eed2a442d3491a7c1848b8e5087d68adb
philipptrenz <mail@philipptrenz.de>
2018-03-17 18:50:05 +0100

    added command to disable display power management to start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 # run photo booth and restart for the case it crashes
 #while true; do
+xset -dpms ; xset s off # turn display power management off which causes display sleep
 sudo startx /home/pi/photo-booth/node_modules/.bin/electron /home/pi/photo-booth/ -- -nocursor
 #done

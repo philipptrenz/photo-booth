7a60fab64f25f27637def0ae1e27497215fcb347
philipptrenz <mail@philipptrenz.de>
2018-04-01 19:30:46 +0200

    added licence header

--- app/camera.js
+++ app/camera.js
@@ -1,3 +1,23 @@
+/* 
+ * This file is part of "photo booth" 
+ * Copyright (c) 2018 Philipp Trenz
+ *
+ * For more information on the project go to
+ * <https://github.com/philipptrenz/photo-booth>
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import sharp from 'sharp';
 import gphoto2 from 'gphoto2';
 
@@ -11,11 +31,6 @@ class Camera {
 
 		// Negative value or undefined will disable logging, levels 0-4 enable it.
 		this.GPhoto.setLogLevel(-1);
-		/*
-		this.GPhoto.on('log', function (level, domain, message) {
-		  console.log(domain, message);
-		});
-		*/
 	}
 
 	/*
@@ -41,7 +56,6 @@ class Camera {
 					}
 				});
 			}
-
 		});
 	}
 

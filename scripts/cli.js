2f2b8d6036558f37909fba2580c11b80e6de47fa
philipptrenz <mail@philipptrenz.de>
2018-04-10 13:17:55 +0200

    added cli.js

---
+++ scripts/cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var spawn = require('child_process').spawn
+var electron = require('electron-prebuilt')
+var join = require('path').join
+
+var app = spawn(electron, ['../main.js'], { 
+  stdio: 'inherit',
+  cwd: join(__dirname) 
+}).on('exit', function (i, m) {
+  process.exit()
+})
